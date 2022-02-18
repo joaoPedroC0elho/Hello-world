cb3ff371416c3194c9a105cc1392446e10c470a6
joaoPedroC0elho <96500747+joaoPedroC0elho@users.noreply.github.com>
2022-02-18 10:07:00 -0300

    Add files via upload

---
+++ car average.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+   
+  float x, y;
+  printf("Input total distance in km");
+  scanf("%d", &x);
+  printf("Input total fuel spent in liters:");
+  scanf("%f", &y);
+  printf("Average comsumption (km\lt) %.3f", x/y);
+  printf("\n");
+
+
+}
+
+
+
+ 
+ 
