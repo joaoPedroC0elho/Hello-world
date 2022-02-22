e152045d2af57d9ed21e74771a3425adfbf7398d
joaoPedroC0elho <96500747+joaoPedroC0elho@users.noreply.github.com>
2022-02-22 09:11:33 -0300

    Add files via upload

---
+++ caluladora.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main(){
+	
+	char opcao;
+	
+	int n1, n2, n3, x1, x2, delta;
+	
+	float result;
+	
+	printf("escolha um opcao(+, -, *, /, bhask)para uma operacao em C\n")
+	
+	printf
+	
+	
+	
+	
